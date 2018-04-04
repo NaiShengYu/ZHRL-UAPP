d07a4bf64ef23b49f62652fbbf2d717425202f4c
bradleyyuan <bradleyyuan@DEV12>
2018-04-04 10:35:13 +0800

    修改添加站点逻辑顺序

--- AepApp/View/AddSite.xaml.cs
+++ AepApp/View/AddSite.xaml.cs
@@ -16,14 +16,24 @@ namespace AepApp.View
 	public partial class AddSite : ContentPage
 	{
         private String result;
-		public AddSite ()
+        
+
+        public AddSite ()
 		{
 			InitializeComponent ();          
             this.Title = "添加站点";
             ToolbarItems.Add(new ToolbarItem("添加", "",  () =>
             {
-                //请求该站点数据
-                reqSiteData();
+                var aaaa = this.siteAddr.Text;
+                if (aaaa == null || aaaa.Length == 0)
+                {
+                    CrossHud.Current.ShowError(message: "请输入站点IP", timeout: new TimeSpan(0, 0, 2), cancelCallback: cancel);
+                }
+                else {
+                    CrossHud.Current.Show("加载中...");
+                    //请求该站点数据
+                    reqSiteData();
+                }                         
                // App.Database.SaveItemAsync();
                //await Navigation.PopAsync(false);
 
@@ -35,12 +45,7 @@ namespace AepApp.View
             BackgroundWorker wrk = new BackgroundWorker();
             wrk.DoWork += (sender1, e1) =>
             {
-                Console.WriteLine("添加站点");
-                var aaaa = this.siteAddr.Text;
-                if(aaaa.Length == 0 || this.siteAddr.Text ==null){
-                    CrossHud.Current.ShowError(message: "请输入站点IP");
-                    return;
-                }
+               // Console.WriteLine("添加站点");                               
                 string uri = "https://"+this.siteAddr.Text+"/api/login/getstationName?stationurl="+this.siteAddr.Text;               
                 result = EasyWebRequest.sendGetHttpWebRequestWithNoToken(uri);          
             };
@@ -48,9 +53,19 @@ namespace AepApp.View
             {
                 Console.WriteLine("ex:" + result);
                 //添加站点
-
+                CrossHud.Current.Dismiss();
             };
             wrk.RunWorkerAsync();
+        }
+
+        private void cancel()
+        {
+            CrossHud.Current.Dismiss();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {           
+            return true;
         }
     }
 }
