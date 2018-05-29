4e8dcabc7d8c4790476fc317a05428520415cee5
stephenyu <123456>
2018-05-29 11:06:51 +0800

    提交新安卓环境

---
+++ AepApp/View/EnvironmentalEmergency/WatchListInfoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AepApp.View.EnvironmentalEmergency
+{
+    public partial class WatchListInfoPage : ContentPage
+    {
+        public WatchListInfoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AepApp/View/EnvironmentalEmergency/WatchListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AepApp.View.EnvironmentalEmergency
+{
+    public partial class WatchListPage : ContentPage
+    {
+        public WatchListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
