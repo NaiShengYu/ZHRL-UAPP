7ae00aa115ba61eae712254fa3b772212ad2b7f2
bchan <bchan@DEV00>
2018-06-01 16:17:54 +0800

    Added width control of master view in masterdetailpage

--- AepApp/View/MasterAndDetailPage.xaml.cs
+++ AepApp/View/MasterAndDetailPage.xaml.cs
@@ -8,11 +8,31 @@ namespace AepApp.View
 {
     public partial class MasterAndDetailPage : MasterDetailPage
     {
+        public readonly static BindableProperty WidthRatioProperty =
+            BindableProperty.Create("WidthRatio",
+            typeof(float),
+            typeof(MasterAndDetailPage),
+            (float)0.6);
+
+        public float WidthRatio
+        {
+            get
+            {
+                return (float)GetValue(WidthRatioProperty);
+            }
+            set
+            {
+                SetValue(WidthRatioProperty, value);
+            }
+        }
+
+
+
         public MasterAndDetailPage()
         {
             InitializeComponent();
             Master = new MasterPage(this);
-
+            Master.WidthRequest = 100;
             //Detail = new NavigationPage(new AirPage());
 
             Detail = new NavigationPage(new EmergencyAccidentPage());
---
+++ iOS/MasterAndDetailPageRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AepApp.iOS;
+using AepApp.View;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MasterAndDetailPage), typeof(MasterAndDetailPageRenderer))]
+namespace AepApp.iOS
+{
+    public class MasterAndDetailPageRenderer : MyPhoneMasterDetailRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            
+        }
+    }
+}
