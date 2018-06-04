50fdc091f717aa6f2aa6f7b800bbc42fedf4682e
stephenyu <123456>
2018-06-04 14:22:59 +0800

    重新上传git上上传失败的文件

---
+++ AepApp/Models/ChemicalModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AepApp.Models
+{
+	public class ChemicalModel
+	{
+        public string name { get; set; }
+        public string Yname { set; get; }
+        public string type { set; get; }
+        public string TypeNum { set; get; }
+    }
+}
