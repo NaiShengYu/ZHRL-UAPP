8ff89cd0910ad6cc198f896f54409a0cb99ed02f
NaiShengYu <2495024529@qq.com>
2018-08-13 09:31:01 +0800

    添加网格化管理

---
+++ AepLib/View/Gridding/ReportEventPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AepApp.View.Gridding
+{
+    public partial class ReportEventPage : ContentPage
+    {
+        public ReportEventPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
