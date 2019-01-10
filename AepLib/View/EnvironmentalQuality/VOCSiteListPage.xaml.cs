db6c58ea4b46908ab806c6e47b75849710bf6a69
NaiShengYu <2495024529@qq.com>
2019-01-10 15:57:50 +0800

    Void

--- AepLib/View/EnvironmentalQuality/VOCSiteListPage.xaml.cs
+++ AepLib/View/EnvironmentalQuality/VOCSiteListPage.xaml.cs
@@ -103,7 +103,7 @@ namespace AepApp.View.EnvironmentalQuality
                     }
                     if (dataList.Count >= result.Totals) _isEnd = true;
                     else _isEnd = false;
-                    if (total != 0) Title = App.moduleConfigENVQ.menuVOCLabel + "( " + total + " )";
+                    if (total != 0) Title = App.moduleConfigENVQ.menuVOCLabel + "（" + total + "）";
                     else Title = App.moduleConfigENVQ.menuVOCLabel;
 
                 }
