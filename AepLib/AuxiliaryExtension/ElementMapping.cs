91f6f1b56e0e8b3565cb77aa5caf8b0db390d746
NaiShengYu <2495024529@qq.com>
2019-04-26 15:54:11 +0800

    失败的添加

--- AepLib/AuxiliaryExtension/ElementMapping.cs
+++ AepLib/AuxiliaryExtension/ElementMapping.cs
@@ -35,18 +35,5 @@ namespace AepApp.AuxiliaryExtension
             return d;
         }
 
-        public ElementMapping()
-        {
-            // Register for connectivity changes, be sure to unsubscribe when finished
-            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
-        }
-
-
-        void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
-        {
-            var access = e.NetworkAccess;
-            var profiles = e.Profiles;
-        }
-
     }
 }
